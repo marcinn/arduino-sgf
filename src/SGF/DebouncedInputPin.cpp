895585af9d4dfebd86fec0a3ef1b1b3e176618c0
Marcin Nowak <marcin.j.nowak@gmail.com>
2026-03-02 01:35:33 +0100

    debounceinputpin fix

--- src/SGF/DebouncedInputPin.cpp
+++ src/SGF/DebouncedInputPin.cpp
@@ -1,8 +1,17 @@
 #include "DebouncedInputPin.h"
 
+DebouncedInputPin::DebouncedInputPin(uint8_t pinNumber, bool activeLow) : InputPin(pinNumber, activeLow) {
+    stablePressedFlag = InputPin::isActive();
+    lastRawPressed = stablePressedFlag;
+    lastRawChangeMs = millis();
+}
+
 void DebouncedInputPin::update() {
+    InputPin::update();
     bool rawNow = InputPin::isActive();
     if (rawNow == stablePressedFlag) {
+        lastRawPressed = rawNow;
+        lastRawChangeMs = millis();
         return;
     }
     uint32_t nowMs = millis();
